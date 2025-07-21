5a7231b0c509ee46230476d610b80c99164e72a8
Krishan Mohan <krishanmohan3501@gmail.com>
2025-07-21 22:32:16 +0530

    user , video models, bcrypt and jwt access and refress token setup cutom method

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // searching field me optimised ho jata hia mongodb me
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url ka use karenge
+      required: true,
+    },
+
+    coverImage: {
+      type: String, // cloudinary url rahega yaha
+    },
+    watchHistory: [
+      // ye array lenge
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String, // aise password ko nhi rakhna chahiye agar databse leak ho gya to , lekin encrypt kar diyye to dono string ko match kaise karenge
+      required: [true, "Password is required"],
+    },
+    refeshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// khud ka custome method to match the password
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password); // this will return true or false
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
--- backend/src/utils/asynchandler.js
+++ backend/src/utils/asynchandler.js
@@ -2,9 +2,7 @@ const asynchandler = (requestHandler)=>{
     (req, res, next)=>{
             Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
     }
-
 }
-
 export { asynchandler }
 
 
